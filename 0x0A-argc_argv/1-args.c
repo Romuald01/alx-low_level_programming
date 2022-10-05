18f09c336647532dd57ff82d2c6f8279c03d0158
romuald <romualdadjimagbe2018@gmail.com>
2022-10-05 03:48:12 -0700

    updating argc and argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -print the name number of arguments passed to it
+ * @argc: number of arguments passed to the function
+ * @argv:argument vector of pointers to string
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%\d", argc - 1);
+	return (0);
+}
