7109773581eedd60c413025f2726f6b85fe2ebd2
romuald <romualdadjimagbe2018@gmail.com>
2022-10-05 04:01:38 -0700

    updating argc and argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -print all arguments it receives
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointer to string
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
