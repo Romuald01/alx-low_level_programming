6a314377fc60637a8f6a431e105416d283536cf9
romuald <romualdadjimagbe2018@gmail.com>
2022-10-05 03:35:33 -0700

    updating argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,14 @@
-#include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+/**
+ * main -prints the name of the program
+ * @argc: number of arguments passed to the function
+ * @argv:argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("Romuald\n");
-
+	printf("%n", argv[0]);
 	return (0);
 }
