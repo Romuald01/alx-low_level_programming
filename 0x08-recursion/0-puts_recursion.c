2c716d9170fe42fac03115a0818259e8e3efc156
romuald <romualdadjimagbe2018@gmail.com>
2022-10-17 15:50:57 -0700

    updating recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,25 @@
-recursion project
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print a string, followed by new line.
+ * @s: the string.
+ * Return: void.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		printf("\n");
+	printf("%c", *s);
+	_puts_recursion(++s);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("puts with recursion");
+	return (0);
+}
